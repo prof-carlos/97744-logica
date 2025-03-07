f0a397601669ac1e5348f6c27d2eb4073e135e03
prof-carlos <128708765+prof-carlos@users.noreply.github.com>
2025-03-07 16:36:56 +0000

    save

--- 2. estruturas_condicionais/17. pagamento.py
+++ 2. estruturas_condicionais/17. pagamento.py
@@ -17,12 +17,27 @@ match forma_de_pagamento:
     case 1: 
         # Obtendo o valor do desconto de 10%.
         desconto = valor_produto * 0.10
-        
+        valor_com_desconto = valor_produto - desconto
+
+        # Exibindo resultados.
+        print(f"\nValor do produto: R$ {valor_produto}")
+        print(f"Forma de pagamento: à vista")
+        print(f"Valor do desconto: R${desconto}")
+        print(f"Total a pagar: R$ {valor_com_desconto}")
+
     case 2: 
-        parcelas = int(input("Digite a quantidade de parcelas: "))
+        quantidade_parcelas = int(input("Digite a quantidade de parcelas: "))
+        if quantidade_parcelas >= 1 and quantidade_parcelas <= 6:
+            valor_da_parcela = valor_produto / quantidade_parcelas
         
+            # Exibindo resultados.
+            print(f"\nValor do produto: R$ {valor_produto}")
+            print(f"Forma de pagamento: à prazo")
+            print(f"Quantidade de parcelas: {quantidade_parcelas}")
+            print(f"Valor por parcela: R$ {valor_da_parcela:.2f}")
+            print(f"Total à prazo: R$ {valor_produto}")
+        else:
+            print("Opção inválida.")
+
     case _:
         print("Opção inválida.")
-
-# 4 - Exibindo resultados.
-print()
